d67e9d2e8ba5f93795d7d3a3ef0b453c590c1991
NgumNfor <ngumjuden1234@gmail.com>
2022-03-19 17:37:32 -0400

    added

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@
 module "vpcmodule" {
   source = "git::git@github.com:NGUMNFOR/30172022march.git//vpcmodule"
 
-  vpccidr = "10.0.0.0/16"
+  vpccidr = "200.0.0.0/16"
   websubnetnames  = ["web1"] 
   Appsubnames     = ["app1"]
-  websubnet_cidr  = [for each in range(1,225,2) : cidrsubnet("10.0.0.0/16", 8, each)]
-  appsubnet_cidr  = [for each in range(0,225,2) : cidrsubnet("10.0.0.0/16", 8, each)]
+  websubnet_cidr  = [for each in range(1,225,2) : cidrsubnet("200.0.0.0/16", 8, each)]
+  appsubnet_cidr  = [for each in range(0,225,2) : cidrsubnet("200.0.0.0/16", 8, each)]
 }
