36def3d8946b3864e40febe73383349b9af177fb
Ngum Jude Nfor <97320396+NGUMNFOR@users.noreply.github.com>
2022-03-19 16:51:06 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+
+
+
+module "vpcmodule" {
+  source = "git::git@github.com:NGUMNFOR/30172022march.git//vpcmodule"
+
+  vpccidr = "10.0.0.0/16"
+  websubnetnames  = ["web1"] 
+  Appsubnames     = ["app1"]
+  websubnet_cidr  = [for each in range(1,225,2) : cidrsubnet("10.0.0.0/16", 8, each)]
+  appsubnet_cidr  = [for each in range(0,225,2) : cidrsubnet("10.0.0.0/16", 8, each)]
+}
